4b0d25957b6c30c66033f4e41e41daa931a0e626
ap vpn <ap@vpninfotech.com>
2018-05-10 17:05:50 +0530

    source added

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
+})
+export class AppComponent {
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+  selectAllToDo(todos){
+    todos.forEach(todo => {
+      console.log(todo);
+      this.toggleTodoComplete(todo);
+    });
+  }
+
+  deleteAllToDo(todos){
+    todos.forEach(todo => {
+        if(todo.complete){
+            console.log(todo);
+            this.removeTodo(todo);
+        }
+    });
+  }
+}
